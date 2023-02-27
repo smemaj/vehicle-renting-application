3d9932250d713145f294f6897e7821c669577aa4
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-02-27 23:29:15 +0100

    Initialize project

---
+++ src/main/java/com/spring/vehiclerenting/VehicleRentingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.vehiclerenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentingApplication.class, args);
+	}
+
+}
