aaf89eba4a1bba94a78912fa45b8259e1f3399db
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-02-28 01:02:13 +0100

    update key and expiration

--- src/main/java/com/spring/vehiclerenting/security/jwt/JwtUtils.java
+++ src/main/java/com/spring/vehiclerenting/security/jwt/JwtUtils.java
@@ -41,13 +41,13 @@ public class JwtUtils {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException e) {
-            logger.error("Invalid JWT signature: {}", e.getMessage());
+            logger.error("JWT signature not valid: {}", e.getMessage());
         } catch (MalformedJwtException e) {
-            logger.error("Invalid JWT token: {}", e.getMessage());
+            logger.error("JWT token not valid: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
-            logger.error("JWT token is expired: {}", e.getMessage());
+            logger.error("Token is expired: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
-            logger.error("JWT token is unsupported: {}", e.getMessage());
+            logger.error("Unsupported token: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
             logger.error("JWT claims string is empty: {}", e.getMessage());
         }
